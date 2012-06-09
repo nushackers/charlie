b53ce71de5a082c91975da61269b5af685522d40
Eli James <ejames@pandamian.com>
2012-06-09 22:10:38 +0800

    Initial commit, with .gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Charlie::Application.config.secret_token = '044111a3c306217aa3927d0cb816314a6b208e5f40cf9f1c084316ef34a7fd16ffcf53b9779008d4027902ebbf16f02926d1b0e7eaaf4c1c95861c4fe23c0f04'
